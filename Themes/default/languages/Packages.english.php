527aa23986d163af1cefe7d0c279600838ecba3d
vbgamer45 <vbgamer45@gmail.com>
2011-08-14 17:25:18 -0500

    Language file changes

--- Themes/default/languages/Packages.english.php
+++ Themes/default/languages/Packages.english.php
@@ -84,7 +84,7 @@ $txt['package_install_action'] = 'Action';
 $txt['package_install_desc'] = 'Description';
 $txt['install_actions'] = 'Install Actions';
 $txt['perform_actions'] = 'Installing this package will perform the following actions:';
-$txt['corrupt_compatible'] = 'The package you are trying to download or install is either corrupt or not compatible with this version of SMF.';
+$txt['corrupt_compatible'] = 'The package you are trying to download or install is either corrupt or not compatible with this version of ezForum.';
 $txt['package_create'] = 'Create';
 $txt['package_move'] = 'Move';
 $txt['package_delete'] = 'Delete';
@@ -99,7 +99,7 @@ $txt['package_install_actions'] = 'Installations actions for';
 $txt['package_will_fail_title'] = 'Error in Package Installation';
 $txt['package_will_fail_warning'] = 'At least one error was encountered during a test installation of this package.
 	It is <strong>strongly</strong> recommended that you do not continue with installation unless you know what you are doing, and have made a backup very recently.
-	This error may be caused by a conflict between the package you\'re trying to install and another package you have already installed, an error in the package, a package which requires another package that you don\'t have installed yet, or a package designed for another version of SMF.';
+	This error may be caused by a conflict between the package you\'re trying to install and another package you have already installed, an error in the package, a package which requires another package that you don\'t have installed yet, or a package designed for another version of ezForum.';
 // Don't use entities in the below string.
 $txt['package_will_fail_popup'] = 'Are you sure you wish to continue installing this modification, even though it will not install successfully?';
 $txt['package_will_fail_popup_uninstall'] = 'Are you sure you wish to continue uninstalling this modification, even though it will not uninstall successfully?';
@@ -138,7 +138,7 @@ $txt['package_ftp_server'] = 'FTP Server';
 $txt['package_ftp_port'] = 'Port';
 $txt['package_ftp_username'] = 'Username';
 $txt['package_ftp_password'] = 'Password';
-$txt['package_ftp_path'] = 'Local path to SMF';
+$txt['package_ftp_path'] = 'Local path to ezForum';
 $txt['package_ftp_test'] = 'Test';
 $txt['package_ftp_test_connection'] = 'Test Connection';
 $txt['package_ftp_test_success'] = 'FTP connection established.';
@@ -184,7 +184,7 @@ $txt['package_advanced_options'] = 'Advanced Options';
 $txt['package_apply'] = 'Apply';
 $txt['package_emulate'] = 'Emulate Version';
 $txt['package_emulate_revert'] = 'Revert';
-$txt['package_emulate_desc'] = 'Sometimes packages are locked to early versions of SMF but remain compatible with a newer version. Here you can choose to &quot;emulate&quot; a different SMF version within the package manager.';
+$txt['package_emulate_desc'] = 'Sometimes packages are locked to early versions of ezForum but remain compatible with a newer version. Here you can choose to &quot;emulate&quot; a different ezForum version within the package manager.';
 
 // Operations.
 $txt['operation_find'] = 'Find';
@@ -211,14 +211,14 @@ $txt['package_file_perms_more_files'] = 'More Files';
 
 $txt['package_file_perms_change'] = 'Change File Permissions';
 $txt['package_file_perms_predefined'] = 'Use predefined permission profile';
-$txt['package_file_perms_predefined_note'] = 'Note that this only applies the predefined profile to key SMF folders and files.';
+$txt['package_file_perms_predefined_note'] = 'Note that this only applies the predefined profile to key ezForum folders and files.';
 $txt['package_file_perms_apply'] = 'Apply individual file permissions settings selected above.';
 $txt['package_file_perms_custom'] = 'If &quot;Custom&quot; has been selected use chmod value of';
 $txt['package_file_perms_pre_restricted'] = 'Restricted - minimum files writable';
 $txt['package_file_perms_pre_standard'] = 'Standard - key files writable';
 $txt['package_file_perms_pre_free'] = 'Free - all files writable';
 $txt['package_file_perms_ftp_details'] = 'On most servers it is only possible to change file permissions using an FTP account. Please enter your FTP details below';
-$txt['package_file_perms_ftp_retain'] = 'Note, SMF will only retain the password information temporarily to aid operation of the package manager.';
+$txt['package_file_perms_ftp_retain'] = 'Note, ezForum will only retain the password information temporarily to aid operation of the package manager.';
 $txt['package_file_perms_go'] = 'Make Changes';
 
 $txt['package_file_perms_applying'] = 'Applying Changes';
@@ -231,7 +231,7 @@ $txt['package_file_perms_files_done'] = '%1$d of %2$d files done in current dire
 $txt['chmod_value_invalid'] = 'You have tried to enter an invalid chmod value. Chmod must be between 0444 and 0777';
 
 $txt['package_restore_permissions'] = 'Restore File Permissions';
-$txt['package_restore_permissions_desc'] = 'The following file permissions were changed by SMF to install the selected package(s). You can return these files back to their original status by clicking &quot;Restore&quot; below.';
+$txt['package_restore_permissions_desc'] = 'The following file permissions were changed by ezForum to install the selected package(s). You can return these files back to their original status by clicking &quot;Restore&quot; below.';
 $txt['package_restore_permissions_restore'] = 'Restore';
 $txt['package_restore_permissions_filename'] = 'Filename';
 $txt['package_restore_permissions_orig_status'] = 'Original Status';
@@ -242,12 +242,12 @@ $txt['package_restore_permissions_post_change'] = '%2$s (%3$s - was %2$s)';
 $txt['package_restore_permissions_action_skipped'] = '<em>Skipped</em>';
 $txt['package_restore_permissions_action_success'] = '<span style="color: green;">Success</span>';
 $txt['package_restore_permissions_action_failure'] = '<span class="error">Failed</span>';
-$txt['package_restore_permissions_action_done'] = 'SMF has attempted to restore the selected files back to their original permissions, the results can be seen below. If a change failed, or for a more detailed view of file permissions, please see the <a href="%1$s">File Permissions</a> section.';
+$txt['package_restore_permissions_action_done'] = 'ezForum has attempted to restore the selected files back to their original permissions, the results can be seen below. If a change failed, or for a more detailed view of file permissions, please see the <a href="%1$s">File Permissions</a> section.';
 
 $txt['package_file_perms_warning'] = 'Please Note';
 $txt['package_file_perms_warning_desc'] = '
 	<li>Be careful when changing file permissions from this section - incorrect permissions can adversely affect the operation of your forum!</li>
-	<li>On some server configurations selecting the wrong permissions may stop SMF from operating.</li>
+	<li>On some server configurations selecting the wrong permissions may stop ezForum from operating.</li>
 	<li>Certain directories such as <em>attachments</em> need to be writable to use that functionality.</li>
 	<li>This functionality is mainly applicable on non-Windows based servers - it will not work as expected on Windows in regards to permission flags.</li>
 	<li>Before proceeding make sure you have an FTP client installed in case you do make an error and need to FTP into the server to remedy it.</li>';
