0ffd3213568e6ee956c3c0dae2d6f42ebeaeeca8
vbgamer45 <vbgamer45@gmail.com>
2013-02-07 21:33:23 -0600

    Pretty Urls Updates

--- Sources/PrettyUrls.php
+++ Sources/PrettyUrls.php
@@ -1,4 +1,38 @@
 <?php
+/*
+Pretty URLs licence
+===================
+Copyright (c) 2006-2009 The Pretty URLs Contributors (http://code.google.com/p/prettyurls/source/browse/trunk/CONTRIBUTORS)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+this list of conditions, and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions, and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+* Neither the name of the author of this software nor the name of
+contributors to this software may be used to endorse or promote products
+derived from this software without specific prior written consent.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+
+
 //	Version: 1.0RC; PrettyUrls
 
 if (!defined('SMF'))
@@ -24,10 +58,6 @@ function PrettyInterface()
 	$context['pretty']['chrome'] = array(
 		'admin' => true,
 		'menu' => array(
-			'news' => array(
-				'href' => $scripturl . '?action=admin;area=pretty',
-				'title' => $txt['pretty_chrome_menu_news'],
-			),
 			'settings' => array(
 				'href' => $scripturl . '?action=admin;area=pretty;sa=settings',
 				'title' => $txt['pretty_chrome_menu_settings'],
@@ -44,35 +74,13 @@ function PrettyInterface()
 	$subActions = array(
 		'filters' => 'pretty_edit_filters',
 		'maintenance' => 'pretty_maintenance',
-		'news' => 'pretty_news',
 		'settings' => 'pretty_manage_settings',
 		'test' => 'pretty_test_rewrites',
 	);
 	if (isset($_REQUEST['sa']) && isset($subActions[$_REQUEST['sa']]))
 		call_user_func($subActions[$_REQUEST['sa']]);
 	else
-		pretty_news();
-}
-
-//	News and mod information
-function pretty_news()
-{
-	global $context, $scripturl, $settings, $txt;
-
-	//	This page needs some extra javascript :)
-	$context['html_headers'] .= '
-	<script type="text/javascript">var chrome = {
-		downloadTxt: "' . $txt['pretty_download'] . '",
-		pmUrl: "' . $scripturl . '?action=admin;area=packages;sa=download;get;sesc=' . $context['session_id'] . ';package=",
-		upgradeTxt: "' . $txt['pretty_upgrade'] . '"
-	};</script>
-	<script type="text/javascript" src="' . $settings['default_theme_url'] . '/pretty/chrome.js"></script>
-	<script type="text/javascript" src="http://prettyurls.googlecode.com/svn/trunk/news.js"></script>';
-
-	$context['page_title'] = $txt['pretty_chrome_page_title_news'];
-	$context['sub_template'] = 'pretty_news';
-	$context['pretty']['chrome']['page_title'] = $txt['pretty_chrome_title_news'];
-	$context['pretty']['chrome']['caption'] = $txt['pretty_chrome_caption_news'];
+		pretty_manage_settings();
 }
 
 //	An interface to manage the settings and filters
