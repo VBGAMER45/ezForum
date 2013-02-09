a43603e7109c4cb3effe143f1bd867fbbd5cca4d
vbgamer45 <vbgaemer45@gmail.com>
2013-02-09 00:27:20 -0600

    Add Stars to Profile Page

--- Themes/default/Profile.template.php
+++ Themes/default/Profile.template.php
@@ -64,10 +64,25 @@ function template_summary()
 	<div id="basicinfo">
 		<div class="windowbg">
 			<span class="topslice"><span></span></span>
-			<div class="content flow_auto">
-				<div class="username"><h4>', $context['member']['name'], ' <span class="position">', (!empty($context['member']['group']) ? $context['member']['group'] : $context['member']['post_group']), '</span></h4></div>
+			<div class="content flow_auto">';
+            /*
+            Copyright (c) 2011, Masterd
+            
+            Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+            
+            Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+            Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+            The names of the contributors may be not used to endorse or promote products derived from this software without specific prior written permission.
+            
+            THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+            */
+            
+            echo '
+				<div class="username"><h4>', $context['member']['name'], ' <span class="position">', (!empty($context['member']['group']) ? $context['member']['group'] : $context['member']['post_group']), '<br />', (!empty($context['member']['group_stars']) ? $context['member']['group_stars'] : ''), '</span></h4></div>
 				', $context['member']['avatar']['image'], '
 				<ul class="reset">';
+            // End Masterd    
+                
 
 	// What about if we allow email only via the forum??
 	if ($context['member']['show_email'] === 'yes' || $context['member']['show_email'] === 'no_through_forum' || $context['member']['show_email'] === 'yes_permission_override')
